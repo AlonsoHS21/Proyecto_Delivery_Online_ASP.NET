d9038f17f1611faa12254629bd6b7d407e386901
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-04-03 14:07:04 -0300

    Insert en la base de datos

--- Database_SQL/Create_And_Inserts.sql
+++ Database_SQL/Create_And_Inserts.sql
@@ -1,9 +1,9 @@
 -- use master
 -- drop database Huarcaya_Delivery_DB
 
--- create database Huarcaya_Delivery_DB
+create database Huarcaya_Delivery_DB
 go
---use Huarcaya_Delivery_DB
+use Huarcaya_Delivery_DB
 go
 create table Usuarios(
 	ID smallint identity(1,1),
@@ -15,13 +15,14 @@ create table Pedidos(
 	ID smallint identity(1,1),
 	ID_Usuario smallint not null,
 	PrecioTotal int not null,
+	Comentarios varchar(100) null,
 	Fecha date not null,
 	Estado bit not null
 )
 go
 create table Datos_Personales(
 	ID_Usuario smallint not null,
-	Telefono_Movil int null,
+	Telefono_Movil int not null,
 	Nombre_Y_Apellido varchar(200) not null,
 	Fecha_Nacimiento date not null,
 	Email varchar(100) not null,
@@ -37,3 +38,13 @@ alter table Datos_Personales add foreign key (ID_Usuario) references Usuarios(ID
 go
 alter table Pedidos add foreign key (ID_Usuario) references Usuarios(ID)
 go
+-- Inserts into 
+----		Tabla Usuarios		----
+insert into Usuarios(Email,Contrasenia) values ('AlonsoHS20@hotmail.com','Alonsolullaby1')
+insert into Usuarios(Email,Contrasenia) values ('alonsohua20@gmail.com','Alonsolullaby2')
+----		Tabla Pedidos		----
+insert into Pedidos(ID_Usuario,PrecioTotal,Comentarios,Fecha,Estado)values(1,2000,'','1/04/2021',1)
+insert into Pedidos(ID_Usuario,PrecioTotal,Comentarios,Fecha,Estado)values(2,3000,'','1/04/2021',1)
+----		Tabla Datos Personales			----
+insert into Datos_Personales(ID_Usuario,Telefono_Movil,Nombre_Y_Apellido,Fecha_Nacimiento,Email,Fecha_Cuenta)values(1,'1123390582','Alonso Huarcaya','1/04/1999','AlonsoHS20@hotmail.com','1/01/2021')
+insert into Datos_Personales(ID_Usuario,Telefono_Movil,Nombre_Y_Apellido,Fecha_Nacimiento,Email,Fecha_Cuenta)values(1,'1123390582','Alonso SAnchez','1/04/1999','alonsohua20@gmail.com','1/01/2021')
